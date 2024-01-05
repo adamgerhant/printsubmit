8d46e6814036b5126945d20edac57338f04f2dbc
Adam <adamgerhant@gmail.com>
2024-01-05 12:42:55 -0800

    style changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+"use client"
 import Link from "next/link"
 import submissions from "../public/submissions.png"
 import submissionFocus from "../public/submissionFocus.png"
@@ -11,9 +12,14 @@ export default function Page() {
 
   return (
     <>
-      <div className="flex w-full justify-center">
-        <div className="flex flex-col w-full max-w-[1400px]">
-          <div className="bg-white h-[65px] max-w-[1400px] flex flex-row justify-between items-center">
+      <div className="flex w-full justify-center relative">
+        <div className="absolute w-full">
+          <img src={submissions.src} className="absolute z-0 left-0 w-full opacity-40 top-[65px]"/>
+        </div>
+
+        <div className="absolute flex flex-col w-full max-w-[1500px]">
+
+          <div className="bg-white h-[65px] flex flex-row justify-between items-center">
             <div className="flex flex-row">
               <a href="/" className="font-bold text-[25px] py-1 whitespace-nowrap flex flex-row
               align-center mt-1">
@@ -31,35 +37,45 @@ export default function Page() {
               </a>
             </div>
             <div className="flex w-1/2 items-center justify-end cursor-pointer">
-              <a href="/login" className="font-semibold text-blue-600  py-1 px-2 rounded-lg hover:bg-gray-600/[0.1]">Login</a>
-              <a href="/register?redirect=false" className="font-semibold text-blue-600 ml-2  py-1 px-2 rounded-lg hover:bg-gray-600/[0.1]">Create account</a>
-              <a href="/submissions?continue=guest" className="min-w-[170px] font-semibold text-center py-2 ml-3 mr-4 bg-blue-600 hover:bg-blue-700 text-white rounded-md cursor-pointer">Go to Dashboard</a>
+              
+                {/*
+                <div className="py-2 px-5 bg-gray-200 rounded flex">Signed in as:
+                <p className="ml-2 font-semibold">adamgerhant@gmail.com</p>
+                </div>
+                */}
+                <a href="/login" className="font-semibold text-blue-600 ml-2 py-1 px-2 rounded-lg hover:bg-gray-600/[0.1]">Login</a>
+                <a href="/register?redirect=false" className="font-semibold text-blue-600 ml-2  py-1 px-2 rounded-lg hover:bg-gray-600/[0.1]">Create account</a>
+                
+              <a href="/submissions?continue=guest" className="min-w-[170px] font-semibold text-center px-2 py-2 ml-3 mr-4 bg-blue-600 hover:bg-blue-700 text-white rounded-md cursor-pointer">Go to Dashboard</a>
             </div>
 
           </div>
+          {/*<div className="absolute left-[7%] w-[320px] h-[320px] top-[260px] bg-white rounded-full shadow-[0_0_130px_130px_white]"/>*/}
 
-          <div className="flex flex-row mt-32 relative">
-            <div className="flex flex-col w-[490px] mt-5">
+          <div className="flex flex-row mt-32 ml-4 relative w-min py-5 px-10 bg-white border border-gray-400 rounded-xl shadow-2xl">
+            <div className="flex flex-col w-[490px] mt-2 items-center">
               <h1 className="text-6xl font-semibold mb-8">3D print file submission and management</h1>
               <p className="text-xl font-light"> Create a customizable submission form, manage responses, and send automated emails.</p>
-              <a href="/submissions?continue=guest" className="w-[200px] font-semibold text-center py-3 mt-10 bg-blue-600 text-white text-xl rounded-md cursor-pointer">Go to Dashboard</a>
+              <a href="/submissions?continue=guest" className="w-[200px] font-semibold text-center py-3 mt-6 mb-2 bg-blue-600 text-white text-xl rounded-md cursor-pointer ">Go to Dashboard</a>
             </div>
-            <img src={submissions.src} className="absolute left-[490px] top-[-10px] w-[1040px] shadow-xl ml-4 "/>
           </div>  
 
-          <div className="w-full mt-[300px]">
+
+          
+          <div className="w-full mt-[275px] flex flex-col bg-white shadow-[0_0_50px_50px_rgba(255,255,255,1)]">
             <h2 className="text-4xl font-semibold text-center">Who is this app built for?</h2>
+            <div className="w-[950px] self-center mt-4 mb-[200px]">
+              <p className="text-xl text-center flex flex-col">
+                <span>
+                  3D printing makerspaces that need to efficiently manage and print client submissions. 
+                </span>
+                <span>
+                  For example, staff at libraries or universities, teachers, or businesses with custom print requests.
+                </span>
+              </p>
+            </div>
           </div>
-          <div className="w-[950px] self-center mt-4 mb-[200px]">
-            <p className="text-xl text-center flex flex-col">
-              <span>
-                3D printing makerspaces that need to efficiently manage and print client submissions. 
-              </span>
-              <span>
-                For example, staff at libraries or universities, teachers, or businesses with custom print requests.
-              </span>
-            </p>
-          </div>
+          
 
           <div className="flex flex-row w-full">
             <div className="w-[500px] flex flex-col">
